e48b454d8ccc6fffaf64209ed9d906feb3bf762b
COLAIG <nguyengialoc11@gmail.com>
2025-11-06 21:19:54 +0700

    v2-1

---
+++ data_23/topic19_20__preparing_to_write_about_1.js
@@ -0,0 +1,7 @@
+export const data = [
+  "Before writing, you should research the place thoroughly online.",
+  "Although you might not find exact information about the specific place, you will see similar places displayed.",
+  "It is important to learn about the people, history, and culture of that location",
+  "Try to compare that place with your own country to get a better understanding.",
+  "Making such comparisons helps you describe the place more clearly and make your writing more interesting."
+];
